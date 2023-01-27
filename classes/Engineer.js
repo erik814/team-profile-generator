14f0cbd989983cd6e33c371715bb26805cdcae14
erik814 <erikneafus@gmail.com>
2023-01-26 19:42:28 -0600

    I added 4 classes.

---
+++ classes/Engineer.js
@@ -0,0 +1,16 @@
+const Employee = require('./Employee');
+
+class Engineer extends Employee{
+    constructor(name, id, email, github){
+        super(name, id, email);
+        this.github = github;
+    }
+
+    getGithub(){
+        return `GitHub: ${this.github}`;
+    }
+
+    getRole(){
+        return 'Role: Engineer';
+    }
+}
